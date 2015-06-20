347759261479a2b3d2ee227c3cd768912d02dee5
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-06-20 22:20:24 +0200

    Added specs and code for editing todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -21,6 +21,29 @@ class TodoItemsController < ApplicationController
 
   end
 
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.update_attributes(todo_items_params)
+      flash[:success] = "Saved todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "That todo list item could not be saved."
+      render action: :edit
+    end
+
+  end
+
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge(super)
+  end
+
   private
   def todo_items_params
   	params[:todo_item].permit(:content)
